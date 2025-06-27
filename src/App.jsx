faf7980168f9f4acf51418ed746c96997ccad3a3
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-27 17:18:50 +0530

    Refactor App component to organize imports and improve readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import Navbar from './components/Navbar'
-import './App.css'
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-//  const [count, setCount] = useState(0)
+  //  const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
